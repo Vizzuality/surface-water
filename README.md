7974b57f001430abf08cb9ac6a5549a60c1ce1a1
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-05-24 10:44:12 +0200

    Configure the development tools

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# water-test
+# Earth Genome water detection
+
+# Requirements
+
+- [nodejs](https://nodejs.org/en/)
+
+# Installation
+
+Start by installing the required nodejs packages using `npm` (already bundled with recent nodejs installations)
+
+```
+npm install
+```
+
+Next, start the server by running:
+
+```
+node server.js
+```
+
+You should be able to access your application at [http://localhost:3000/](http://localhost:3000/)
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const config = {
+
+  context: path.join(__dirname, 'app'),
+
+  entry: [
+    './src/index.jsx'
+  ],
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+
+  devtool: 'inline-source-map',
+
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css?sourceMap&modules&importLoaders=1&localI‌​dentName=[name]__[local]___[hash:base64:5]!sass?sourceMap')
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  plugins: [
+    new ExtractTextPlugin('styles.css', {
+        allChunks: true
+    })
+  ]
+
+};
+
+if (!process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        unused: true,
+        dead_code: true,
+        warnings: false
+      }
+    })
+  );
+} else {
+  config.plugins.push(
+    new webpack.NoErrorsPlugin()
+  );
+  config.devtool = 'source-map';
+}
+
+module.exports = config;
