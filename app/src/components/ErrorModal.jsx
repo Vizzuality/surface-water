13990952085e80f16df0b457ff69115f33bd8090
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-05-30 12:51:27 +0200

    Adds the same overlay to the error modal as the search one

--- app/src/components/ErrorModal.jsx
+++ app/src/components/ErrorModal.jsx
@@ -7,12 +7,18 @@ import styles from '../../styles/components/error-modal.scss';
 
 class ErrorModal extends Component {
 
+  onClickOverlay(e) {
+    if(e.target === e.currentTarget) this.props.close();
+  }
+
   render() {
     return (
-      <div className={styles.modal}>
-        <h1 className={styles.title}>We are sorry</h1>
-        <div>{this.props.text}</div>
-        <Button classes={[ styles.button ]} color="white" text="uppercase" click={() => this.props.close()}>Ok</Button>
+      <div className={styles.overlay}  onClick={this.onClickOverlay.bind(this)}>
+        <div className={styles.modal}>
+          <h1 className={styles.title}>We are sorry</h1>
+          <div>{this.props.text}</div>
+          <Button classes={[ styles.button ]} color="white" text="uppercase" click={() => this.props.close()}>Ok</Button>
+        </div>
       </div>
     );
   }
