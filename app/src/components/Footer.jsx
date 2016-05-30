1981140bc213223c54f0f341dec6bef461c47df8
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-05-30 17:28:29 +0200

    Adds the contact email address

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -4,14 +4,23 @@ import React, {Component} from 'react';
 
 import styles from '../../styles/components/footer.scss';
 
+/* We use rot13 to avoid bots spamming the email */
+const contactEmail = 'qna@rnegutrabzr.bet';
+
 class Footer extends Component {
+
+  decodeRot13(str) {
+    return str.replace(/[a-zA-Z]/g, c => {
+      return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+    });
+  }
 
   render() {
     return (
       <div className={styles.footer}>
         <ul className={styles.menu}>
           <li className={styles['menu-item'] + ' ' + styles['menu-item-highlight']}>Know more</li>
-          <li className={styles['menu-item']}>Contact</li>
+          <li className={styles['menu-item']}><a href={`mailto:${this.decodeRot13(contactEmail)}`}>Contact</a></li>
           <li className={styles['menu-item']}>A data visualisation by <a href="http://www.vizzuality.com" target="_blank" rel="noreferrer"><svg className={styles['vizzuality-logo']}><use xlinkHref="#vizzualityIcon" x="0" y="0" /></svg></a></li>
         </ul>
         <img className={styles.share} />
