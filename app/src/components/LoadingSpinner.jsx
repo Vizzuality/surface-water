ccabd4a4eb27600b23c02b2d030daff86bc6d79e
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-05-30 12:53:27 +0200

    Adds an overlay to the loader to prevent actions while fetching the data

--- app/src/components/LoadingSpinner.jsx
+++ app/src/components/LoadingSpinner.jsx
@@ -7,10 +7,14 @@ import styles from '../../styles/components/loading-spinner.scss';
 class LoadingSpinner extends Component {
 
   render() {
-    return <div className={styles.spinner}>
-      <div className={styles.cube1}></div>
-      <div className={styles.cube2}></div>
-    </div>;
+    return (
+      <div className={styles.overlay}>
+        <div className={styles.spinner}>
+          <div className={styles.cube1}></div>
+          <div className={styles.cube2}></div>
+        </div>
+      </div>
+    );
   }
 
 }
