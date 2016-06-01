1ae55b3f20aa34519bca037db1bb0ce3a5f205d7
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-06-01 10:43:56 +0200

    Removes useless actions

--- app/src/actions/map.jsx
+++ app/src/actions/map.jsx
@@ -15,49 +15,12 @@ export function updateURL(params, nextParams) {
   return push(`/map/${url}`);
 };
 
-export function setSelectedArea(area) {
-  return dispatch => {
-    dispatch({
-      type: SELECTED_AREA,
-      payload: area
-    });
-  }
-};
-
 export function setMode(mode) {
   return dispatch => {
     dispatch({
       type: MODE,
       payload: mode
     });
-  }
-};
-
-export function setZoom(zoom) {
-  return dispatch => {
-    dispatch({
-      type: ZOOM,
-      payload: zoom
-    });
-  }
-};
-
-export function setLatLng(latLng) {
-  return dispatch => {
-    dispatch({
-      type: LATLNG,
-      payload: latLng
-    });
-  }
-};
-
-export function setYear(year) {
-  return dispatch => {
-    dispatch({
-      type: YEAR,
-      payload: year
-    });
-    dispatch(updateURL({ year }));
   }
 };
 
@@ -182,15 +145,6 @@ export function setError(text) {
     dispatch({
       type: ERROR,
       payload: text
-    });
-  };
-};
-
-export function toggleSearch(active) {
-  return dispatch => {
-    dispatch({
-      type: SEARCH,
-      payload: { active }
     });
   };
 };
