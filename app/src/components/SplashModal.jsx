f360eee494d2661a7a9efd63d02b6c2f80b86c3c
Dan Hammer <dan.s.hammer@gmail.com>
2016-06-06 00:43:51 -0400

    dependencies; text edits; small fixes

--- app/src/components/SplashModal.jsx
+++ app/src/components/SplashModal.jsx
@@ -12,7 +12,11 @@ class SplashModal extends Component {
       <Modal close={this.props.close}>
         <div className={styles.content}>
           <h1>Water Detection from NASA Landsat data</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean faucibus ex interdum nulla feugiat vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+          <p>
+          
+            This tool measures the extent of global surface water at 30-meter resolution, annually from 1999-2012.  If you are interested in our real-time monitoring project, please contact us!
+          
+          </p>
           <Button color="primary" text="uppercase" click={() => this.props.close()}>Explore the map</Button>
         </div>
       </Modal>
